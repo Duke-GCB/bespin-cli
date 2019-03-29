9825eccc9074e4304469b8ae97ead49553344b0f
Dan Leehr <dan.leehr@duke.edu>
2019-03-29 15:19:53 -0400

    Check for CWL files

--- validate.sh
+++ validate.sh
@@ -7,6 +7,9 @@ set -e
 REPO="$1"
 VERSION="$2"
 
+# Check if .cwl files exist in repo
+ls ${REPO}/*.cwl > /dev/null
+
 # Validate the workflows
 for workflow in $REPO/*.cwl; do
   python bespin/validate_workflow.py $VERSION $workflow
