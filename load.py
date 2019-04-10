4c0c650f0da614953c3e8405a8774b1906a20da5
Dan Leehr <dan.leehr@duke.edu>
2019-04-10 14:37:12 -0400

    Rework load to use a structure of dictionaries and helper functions
    - Includes top-level descriptions
    - Includes info url

--- load.py
+++ load.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 
-import tempfile
-from urllib.request import urlretrieve
-import zipfile
-import os
-from bespin.validate_workflow import main as validate_main
-from bespin.commands import Commands
 from bespin.api import BespinClientErrorException
+from bespin.commands import Commands
 
 # workflow name
 # workflow version string
@@ -15,32 +10,51 @@ from bespin.api import BespinClientErrorException
 # workflow path
 
 
+def make_zipped_version_dict(repo, version_string_no_v, tag):
+  return {
+    'version': 'v{}'.format(version_string_no_v),
+    'url': 'https://github.com/bespin-workflows/{}/archive/v{}.zip'.format(repo, version_string_no_v),
+    'type': 'zipped',
+    'path': '{}-{}/{}.cwl'.format(repo, version_string_no_v, tag),
+    'info_url': 'https://github.com/bespin-workflows/{}/blob/v{}/CHANGELOG.md'.format(repo, version_string_no_v)
+  }
+
+
 workflows = [
-  ('exomeseq-gatk4-preprocessing', 'v1.0.0', 'https://github.com/bespin-workflows/exomeseq-gatk4/archive/v1.0.0.zip', 'zipped', 'exomeseq-gatk4-1.0.0/exomeseq-gatk4-preprocessing.cwl',),
-  ('exomeseq-gatk4-preprocessing', 'v2.0.0', 'https://github.com/bespin-workflows/exomeseq-gatk4/archive/v2.0.0.zip', 'zipped', 'exomeseq-gatk4-2.0.0/exomeseq-gatk4-preprocessing.cwl',),
-  ('exomeseq-gatk4', 'v2.0.0', 'https://github.com/bespin-workflows/exomeseq-gatk4/archive/v2.0.0.zip', 'zipped', 'exomeseq-gatk4-2.0.0/exomeseq-gatk4.cwl',),
+  {'name': 'Whole Exome Sequence preprocessing using GATK4',
+   'tag': 'exomeseq-gatk4-preprocessing',
+   'versions':
+     [
+       make_zipped_version_dict('exomeseq-gatk4','1.0.0','exomeseq-gatk4-preprocessing'),
+       make_zipped_version_dict('exomeseq-gatk4','2.0.0','exomeseq-gatk4-preprocessing'),
+     ],
+   },
+  {'name': 'Whole Exome Sequence analysis using GATK4',
+   'tag': 'exomeseq-gatk4',
+   'versions':
+     [
+       make_zipped_version_dict('exomeseq-gatk4','2.0.0','exomeseq-gatk4'),
+     ],
+   },
+
 ]
 
 def create_workflow(name, tag):
-  c = Commands('bespin-cli-dev','bespin-cli-loader')
-  try:
-    c.workflow_create(name, tag)
-  except BespinClientErrorException:
-    # may already exist
-    pass
+    c = Commands('bespin-cli-dev','bespin-cli-loader')
+    try:
+        c.workflow_create(name, tag)
+    except BespinClientErrorException:
+        # may already exist
+        pass
 
-def create_workflow_version(workflow_values):
-  (name, version, url, workflow_type, path) = workflow_values
-  c = Commands('bespin-cli-dev','bespin-cli-loader')
-  c.workflow_version_create(url, workflow_type, path, 'https://version-info-url.com')
 
-# when creating workflows only try the top-levels
+def create_workflow_version(url, workflow_type, path, version_info_url):
+    c = Commands('bespin-cli-dev','bespin-cli-loader')
+    c.workflow_version_create(url, workflow_type, path, version_info_url)
+
 
 for workflow in workflows:
-  workflow_tag = workflow[0]
-  create_workflow(workflow_tag, workflow_tag)
-  create_workflow_version(workflow)
-
-#   create_version(workfow)
-
+    create_workflow(workflow['name'], workflow['tag'])
+    for version in workflow['versions']:
+        create_workflow_version(version['url'], version['type'], version['path'], version['info_url'])
 
