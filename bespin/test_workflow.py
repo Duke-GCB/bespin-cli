96683744f5b1871c9cac59421cd0310bb231555d
Dan Leehr <dan.leehr@duke.edu>
2019-04-11 14:08:36 -0400

    Updates tests for CwlWorkflowVersionTestCase

--- bespin/test_workflow.py
+++ bespin/test_workflow.py
@@ -18,40 +18,84 @@ class BespinWorkflowValidatorTestCase(TestCase):
 
 class CWLWorkflowVersionTestCase(TestCase):
     def setUp(self):
-        self.cwl_workflow_version = CWLWorkflowVersion(workflow_tag="sometag", url="someurl",
-                                                       description="SomeDesc", version=1)
+        self.cwl_workflow_version = CWLWorkflowVersion(url='someurl',
+                                                       workflow_type='packed',
+                                                       workflow_path='#main',
+                                                       version_info_url='infourl')
 
-    @patch('bespin.workflow.load_tool')
-    def test_create(self, mock_load_tool):
+    @patch('bespin.workflow.CWLWorkflowVersion.validate_workflow')
+    def test_create(self, mock_validate_workflow):
+        # Create should validate the workflow, get the id, and call api.workflow_verisons_post
+        mock_parser = Mock(version='v1', tag='wf-tag', input_fields=['a',], description='SomeDesc')
+        mock_validate_workflow.return_value = mock_parser
         mock_api = Mock()
         mock_api.workflow_get_for_tag.return_value = {'id': 1}
-        response = self.cwl_workflow_version.create(mock_api)
-        mock_fields = mock_load_tool.return_value.inputs_record_schema.get.return_value
-        mock_api.workflow_versions_post.assert_called_with(workflow=1, version_num=1, description="SomeDesc",
-                                                           url="someurl", fields=mock_fields)
+        self.cwl_workflow_version.create(mock_api)
+        mock_api.workflow_get_for_tag.assert_called_with('wf-tag')
+        mock_api.workflow_versions_post.assert_called_with(workflow=1,
+                                                           version='v1',
+                                                           workflow_type='packed',
+                                                           workflow_path='#main',
+                                                           version_info_url='infourl',
+                                                           description='SomeDesc',
+                                                           url="someurl",
+                                                           fields=['a',])
+
+    @patch('bespin.workflow.BespinWorkflowLoader')
+    @patch('bespin.workflow.BespinWorkflowParser')
+    @patch('bespin.workflow.BespinWorkflowValidator')
+    def test_load_and_parse_workflow(self, mock_validator, mock_parser, mock_loader):
+        expected_tag = 'expected-tag'
+        expected_version = 've.x.p'
+        mock_load = mock_loader.return_value.load
+        loaded_and_parsed = self.cwl_workflow_version._load_and_parse_workflow(expected_tag, expected_version)
+
+        # The loader should be instantiated with the workflow and load() called
+        self.assertEqual(mock_loader.call_args, call(self.cwl_workflow_version))
+        self.assertTrue(mock_load.called)
+
+        # The parser should be instantiated with the loaded workflow
+        self.assertEqual(mock_parser.call_args, call(mock_load.return_value))
+
+        # The validator should also be instantiated with the loaded workflow
+        self.assertEqual(mock_validator.call_args, call(mock_load.return_value))
+
+        self.assertTrue(mock_validator.return_value.validate.called)
+        self.assertEqual(mock_validator.return_value.validate.call_args, call(expected_tag, expected_version))
+        self.assertEqual(mock_parser.return_value, loaded_and_parsed)
+
+    @patch('bespin.workflow.BespinWorkflowLoader')
+    @patch('bespin.workflow.BespinWorkflowParser')
+    @patch('bespin.workflow.BespinWorkflowValidator')
+    def test_load_and_parse_workflow_no_validate(self, mock_validator, mock_parser, mock_loader):
+        self.cwl_workflow_version.validate = False
+        self.cwl_workflow_version._load_and_parse_workflow()
+        self.assertFalse(mock_validator.return_value.validate.called)
+
+    @patch('bespin.workflow.BespinWorkflowLoader')
+    @patch('bespin.workflow.BespinWorkflowParser')
+    @patch('bespin.workflow.BespinWorkflowValidator')
+    def test_load_and_parse_override(self, mock_validator, mock_parser, mock_loader):
+        self.cwl_workflow_version.override_tag = 'override-tag'
+        self.cwl_workflow_version.override_version = 'override-version'
+        mock_parser.return_value.tag = 'original-tag'
+        mock_parser.return_value.version = 'original-version'
+        parsed = self.cwl_workflow_version._load_and_parse_workflow()
+        self.assertEqual(parsed.tag, 'override-tag')
+        self.assertEqual(parsed.version, 'override-version')
+
+    @patch('bespin.workflow.CWLWorkflowVersion._load_and_parse_workflow')
+    def test_validate_workflow(self, mock_load_and_parse_workflow):
+        expected_tag = 'expected-tag'
+        expected_version = 've.x.p'
+        validated = self.cwl_workflow_version.validate_workflow(expected_tag, expected_version)
+        self.assertEqual(mock_load_and_parse_workflow.call_args, call(expected_tag, expected_version))
+        self.assertTrue(mock_load_and_parse_workflow.return_value.check_required_fields.called)
+        self.assertEqual(mock_load_and_parse_workflow.return_value, validated)
 
     def test_get_workflow_id(self):
         mock_api = Mock()
         mock_api.workflow_get_for_tag.return_value = {'id': 2}
-        response = self.cwl_workflow_version.get_workflow_id(mock_api)
+        response = self.cwl_workflow_version.get_workflow_id(mock_api, 'wf-tag')
+        mock_api.workflow_get_for_tag.assert_called_with('wf-tag')
         self.assertEqual(response, 2)
-
-    def test_get_version_num(self):
-        mock_api = Mock()
-        self.cwl_workflow_version.version = 2
-        self.assertEqual(self.cwl_workflow_version.get_version_num(mock_api), 2)
-        mock_api.workflow_versions_list.assert_not_called()
-
-        mock_api.workflow_versions_list.return_value = [
-            {'version': 1},
-            {'version': 2},
-        ]
-        self.cwl_workflow_version.version = 'auto'
-        self.assertEqual(self.cwl_workflow_version.get_version_num(mock_api), 3)
-        mock_api.workflow_versions_list.assert_called_with('sometag')
-
-    @patch('bespin.workflow.load_tool')
-    def test_get_fields_from_url(self, mock_load_tool):
-        response = self.cwl_workflow_version.get_fields_from_url()
-        self.assertEqual(response, mock_load_tool.return_value.inputs_record_schema.get.return_value)
-        mock_load_tool.return_value.inputs_record_schema.get.assert_called_with("fields")
